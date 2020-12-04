71798823d04b4c1ea500bf244e4183f005f10435
Victor <viggo10001@hotmail.com>
2020-12-04 12:24:56 +0100

    Refactoring

--- detect.py
+++ detect.py
@@ -365,8 +365,8 @@ def detect(save_img):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='inference/videos/test_trim.mp4', help='source')  # file/folder, 0 for webcam
-    #parser.add_argument('--source', type=str, default='0', help='source')  # file/folder, 0 for webcam
+    #parser.add_argument('--source', type=str, default='inference/videos/test5.mp4', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--source', type=str, default='0', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--output', type=str, default='inference/output', help='output folder')  # output folder
     parser.add_argument('--img-size', type=int, default=1920, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.4, help='object confidence threshold')
